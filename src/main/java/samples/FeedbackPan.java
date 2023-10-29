676f50c84f32135c641486ac9e5698e5a8888743
Robin Becker <rbecker@basis.cloud>
2023-10-29 14:37:01 +0100

    FeedbackPan: Panel hinzugefügt

--- src/main/java/samples/FeedbackPan.java
+++ src/main/java/samples/FeedbackPan.java
@@ -1,5 +1,12 @@
 package samples;
 
-public class FeedbackPan {
+import org.dwcj.component.window.Panel;
 
+public class FeedbackPan {
+    private Panel feedbackP;
+    private Panel formularP;
+    private Panel tableP;
+    private Panel buttonP;
+    private Panel calendarP;
+    private Panel feedtableP;
 }
