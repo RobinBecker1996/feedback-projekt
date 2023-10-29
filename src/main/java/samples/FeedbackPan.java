dab02ab219cd39769d5c68113747ccc89f3268c1
Robin Becker <rbecker@basis.cloud>
2023-10-29 17:23:08 +0100

    Panel zuteilung

--- src/main/java/samples/FeedbackPan.java
+++ src/main/java/samples/FeedbackPan.java
@@ -7,23 +7,36 @@ import org.dwcj.component.window.Panel;
 public class FeedbackPan {
     private Panel feedbackP;
     private Panel formularP;
-    private Panel tableP;
+    private Panel tableEmpP;
+    private Panel topP;
+    private Panel centerP;
+    private Panel bottomP;
     private Panel buttonP;
     private Panel calendarP;
     private Panel feedtableP;
 
-    private ListBox selectLB;
-    private Button newFormular;
+    private Button newFormularbtn;
+    private Button savebtn;
+    private Button editbtn;
 
     public void run() {
         feedbackP = new Panel().addClassName("feedbackP").setVisible(false);
         formularP = new Panel().addClassName("formularP").setVisible(false);
-        tableP = new Panel().addClassName("tableP").setVisible(false);
+        tableEmpP = new Panel().addClassName("tableP").setVisible(false);
+        topP = new Panel().addClassName("topP").setVisible(false);
+        centerP = new Panel().addClassName("centerP").setVisible(false);
+        bottomP = new Panel().addClassName("bottomP").setVisible(false);
         buttonP = new Panel().addClassName("buttonP").setVisible(false);
         calendarP = new Panel().addClassName("calendarP").setVisible(false);
         feedtableP = new Panel().addClassName("feedtableP").setVisible(false);
 
-        selectLB = new ListBox().addClassName("selectLB");
-        newFormular = new Button("New Formular").addClassName("newFormular");
+        newFormularbtn = new Button("New Formular").addClassName("newFormular");
+        savebtn = new Button("savebtn").addClassName("newFormular");
+        editbtn = new Button("editbtn").addClassName("newFormular");
+
+        feedbackP.add(topP, centerP, bottomP);
+        topP.add(tableEmpP);
+        centerP.add(formularP, feedtableP, calendarP);
+        bottomP.add(newFormularbtn, editbtn, savebtn);
     }
 }
